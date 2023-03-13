83eacf88a11d762999366bcc1ba27449838b86c0
int <125262137+StructuredObjects@users.noreply.github.com>
2023-03-13 05:39:09 -0700

    Create discord.php

---
+++ discord.php
@@ -0,0 +1,93 @@
+<?php
+
+class DiscordEmbed
+{
+    private $api;
+    public $title;
+    public $description;
+    public $fields;
+    public $json;
+
+    public function __construct(string $a) 
+    {
+        $this->api = $a;
+    }
+
+    public function _CreateEmbed(string $t, string $dsc, array $fds): void
+    {
+        $this->title = $t;
+        $this->description = $dsc;
+        $this->fields = $fds;
+
+        $this->__create_json();
+
+        echo $this->json. "\n\n";
+        if(count($fds) >= 0) $this->json = $this->__add_fields();
+        echo $this->json. "\n\n";
+    }
+
+    public function send(): bool
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $this->api);
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($curl, CURLOPT_HTTPHEADER, array("Accept: application/json", "Content-Type: application/json"));
+
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $this->json);
+
+        $resp = curl_exec($curl);
+        curl_close($curl);
+        if($resp) { return true; }
+
+        return false;
+    }
+
+    public function __create_json(): void
+    {
+        $this->json = '{
+            "embeds": [{
+              "title": "'. $this->title. '",
+              "description": "'. $this->description .'"
+            }]
+          }';
+    }
+
+    public function __add_fields(): string
+    {
+        $lines = explode("\n", $this->json);
+        $new = '{
+            "embeds": [{
+              "title": "'. $this->title. '",
+              "description": "'. $this->description .'"';
+
+        if(count($this->fields) >= 0) 
+        {
+            $new = "$new,\n\t\t". '"fields": [';
+            $c = 0;
+            foreach($this->fields as $field => $value)
+            {
+                // "field": "value", false,
+                if($c == count($this->fields)-1) 
+                {
+                    $new = $new. '{"name": "'. $field. '", "value": "'. $value[0]. '", "inline": '. $value[1]. '}]';
+                } else { 
+                    $new = $new. '{"name": "'. $field. '", "value": "'. $value[0]. '", "inline": '. $value[1]. '},';
+                }
+                $c++;
+            }
+            $new = $new. "\t\t}]\n";
+        } else { return ""; }
+
+        $new = $new. '}';
+
+        return $new;
+    }
+}
+
+$test = new DiscordEmbed("");
+$test->_CreateEmbed("TEST", "TEST", array("TEST" => ["DES TEST", "true"], "NEW" => ["TEST", "true"]));
+$test->send();
+
+?>
